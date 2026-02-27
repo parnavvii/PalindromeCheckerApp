213d459b752cbcfbdf8e8fbcf5886268ebc4f070
parnavvii <parnaviisharma311@gmail.com>
2026-02-27 09:58:27 +0530

    Character Array Based Palindrome Check

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -9,10 +9,26 @@ when the program starts.
  */
 
 
+
+import java.util.*;
 public class PalindromeCheckerApp {
     public static void main(String [] args){
-        System.out.println("Welcome to Palindrome Checker Management System");
-        System.out.println("Version: 1.0");
-        System.out.println("System initialized successfully");
+        String input = "radar";
+        char[] chars = input.toCharArray();
+        int start = 0;
+        int end = chars.length - 1;
+        boolean isPalindrome = true;
+        while (start < end)
+        {
+            if (chars[start] != chars[end])
+            {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+        System.out.println("Input: " + input);
+        System.out.println("Is it Palindrome? " + isPalindrome);
     }
 }
