a7d300ab38f83e0a0551a48b28efa2287ce50557
parnavvii <parnaviisharma311@gmail.com>
2026-02-27 10:42:07 +0530

    initial

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,26 +1,8 @@
 import java.util.*;
-
-import java.util.*;
 public class PalindromeCheckerApp {
     public static void main(String [] args){
-        String input = "noon";
-        Stack<Character> stack = new Stack<>();
-
-        for (char c : input.toCharArray())
-        {
-            stack.push(c);
-        }
-        boolean isPalindrome = true;
-        for (char c : input.toCharArray())
-        {
-            if (c != stack.pop())
-            {
-                isPalindrome = false;
-                break;
-            }
-        }
-        System.out.println("Input: " + input);
-        System.out.println("Is Palindrome? " + isPalindrome);
-
+        System.out.println("Welcome to Palindrome Checker Management System");
+        System.out.println("Version: 1.0");
+        System.out.println("System initialized successfully");
     }
 }
