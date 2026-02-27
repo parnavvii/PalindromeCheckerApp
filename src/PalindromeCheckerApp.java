70574e36ed5a4398f463fd5a5b3b2b3a74ca1e5d
parnavvii <parnaviisharma311@gmail.com>
2026-02-27 10:06:33 +0530

    Stack-Based Palindrome Checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -8,11 +8,27 @@ when the program starts.
 @Version 1.0
  */
 
-
+import java.util.*;
 public class PalindromeCheckerApp {
     public static void main(String [] args){
-        System.out.println("Welcome to Palindrome Checker Management System");
-        System.out.println("Version: 1.0");
-        System.out.println("System initialized successfully");
+        String input = "noon";
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : input.toCharArray())
+        {
+            stack.push(c);
+        }
+        boolean isPalindrome = true;
+        for (char c : input.toCharArray())
+        {
+            if (c != stack.pop())
+            {
+                isPalindrome = false;
+                break;
+            }
+        }
+        System.out.println("Input: " + input);
+        System.out.println("Is Palindrome? " + isPalindrome);
+
     }
 }
