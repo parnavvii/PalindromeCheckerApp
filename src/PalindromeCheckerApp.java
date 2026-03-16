1d6276f89ed1ffd342378e0d88e6f0f5b2810e94
parnavvii <parnavisharma311@gmail.com>
2026-03-16 09:14:09 +0530

    Recursive Palindrome Checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,24 +1,29 @@
-import java.util.*;
-
 public class PalindromeCheckerApp {
-    public static void main(String[] args)
-    {
-        String input = "level";
-        LinkedList<Character> list = new LinkedList<>();
-        for (char c : input.toCharArray())
-        {
-            list.add(c);
-        }
-        boolean isPalindrome = true;
-        while (list.size() > 1)
-        {
-            if (!list.removeFirst().equals(list.removeLast()))
-            {
-                isPalindrome = false;
-                break;
-            }
-        }
+
+    public static void main(String[] args) {
+
+        String input = "madam";
+
+        boolean result = check(input, 0, input.length() - 1);
+
         System.out.println("Input : " + input);
-        System.out.println("Is Palindrome? : " + isPalindrome);
+        System.out.println("Is Palindrome? : " + result);
+    }
+
+    // Recursive method
+    private static boolean check(String s, int start, int end) {
+
+        // Base condition
+        if (start >= end) {
+            return true;
+        }
+
+        // If mismatch found
+        if (s.charAt(start) != s.charAt(end)) {
+            return false;
+        }
+
+        // Recursive call
+        return check(s, start + 1, end - 1);
     }
 }
