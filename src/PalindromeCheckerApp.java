78082dfc74dc8e1039e441f64c9e45fe166fd082
parnavvii <parnavisharma311@gmail.com>
2026-03-16 09:07:55 +0530

    Linked List Based Palindrome Checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,8 +1,24 @@
 import java.util.*;
+
 public class PalindromeCheckerApp {
-    public static void main(String [] args){
-        System.out.println("Welcome to Palindrome Checker Management System");
-        System.out.println("Version: 1.0");
-        System.out.println("System initialized successfully");
+    public static void main(String[] args)
+    {
+        String input = "level";
+        LinkedList<Character> list = new LinkedList<>();
+        for (char c : input.toCharArray())
+        {
+            list.add(c);
+        }
+        boolean isPalindrome = true;
+        while (list.size() > 1)
+        {
+            if (!list.removeFirst().equals(list.removeLast()))
+            {
+                isPalindrome = false;
+                break;
+            }
+        }
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
     }
 }
